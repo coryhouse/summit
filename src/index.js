558b8c417665bf093a15d1dff9891e6c9352d4fd
Cory House <housecor@gmail.com>
2018-12-06 09:54:55 -0500

    Call hooks version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.Context";
+import App from "./App.Hooks";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import ErrorBoundary from "./ErrorBoundary";
