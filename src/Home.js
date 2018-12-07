07ab9f5fa108358d723efa2c0ba019f5b1ac6c36
Cory House <housecor@gmail.com>
2018-12-07 08:51:50 -0500

    Display API results

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllUsers } from "./api/userApi";
 
 // Functional component
 // In Q1, we can start adding state to functional component.
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers().then(usersResponse => setUsers(usersResponse));
+  }, []);
+
   return (
     <div>
       <h2>Welcome Home</h2>
+      <ul>
+        {users.map(u => (
+          <li key={u.id}>{u.email}</li>
+        ))}
+      </ul>
       <img
         src="/logo.png"
         alt="Summit Logo"
